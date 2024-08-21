3df5a995ce4efe66aa1e9a5e39ca4042b8b4e26b
Anan Amer <ananamer95@gmail.com>
2024-08-21 13:52:09 +0300

    day47 Completed - Amazon_Price_Tracker

---
+++ day47-Amazon_Price_Tracker/main.py
@@ -0,0 +1,57 @@
+from bs4 import BeautifulSoup
+import requests
+from dotenv import load_dotenv
+import smtplib
+import os
+
+load_dotenv("../env/day47_env.env")
+
+SMTP_ADDRESS = os.getenv("SMTP_ADDRESS")
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+
+# url = "https://appbrewery.github.io/instant_pot/"
+url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
+
+# A minimal header would look like this:
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
+
+response = requests.get(url, headers=header)
+
+soup = BeautifulSoup(response.content, "html.parser")
+print(soup.prettify())
+
+# Find the HTML element that contains the price
+price = soup.find(class_="a-offscreen").get_text()
+
+# Remove the dollar sign using split
+price_without_currency = price.split("$")[1]
+
+# Convert to floating point number
+price_as_float = float(price_without_currency)
+print(price_as_float)
+
+# ====================== Send an Email ===========================
+
+# Get the product title
+title = soup.find(id="productTitle").get_text().strip()
+print(title)
+
+# Set the price below which you would like to get a notification
+BUY_PRICE = 100
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is on sale for {price}!"
+
+    with smtplib.SMTP(SMTP_ADDRESS, port=587) as connection:
+        connection.starttls()
+        result = connection.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        connection.sendmail(
+            from_addr=EMAIL_PASSWORD,
+            to_addrs="EMAIL TO SEND TO",
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}".encode("utf-8")
+        )
