3b814492513e67f2be91c7a98620c612ec1ac191
Anan Amer <ananamer95@gmail.com>
2024-09-25 18:33:26 +0300

    day70 Completed - git and version control

--- day70-git_Version_control/main.py
+++ day70-git_Version_control/main.py
@@ -1,1 +1,7 @@
-#day70 started
+#day70
+
+# If you want to dive deeper into Git,
+# including learning about Cherry-Picking,
+# Git Rebase and more I recommend completing the challenges here:
+
+# https://learngitbranching.js.org/
